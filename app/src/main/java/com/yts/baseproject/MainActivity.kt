c72c764275401098a5ff4a6961b49e87b338b7d3
YunTaeSik <sky877kr@gmail.com>
2020-01-25 13:46:06 +0900

    1. 추상클래스 상속 예제 추가

--- app/src/main/java/com/yts/baseproject/MainActivity.kt
+++ app/src/main/java/com/yts/baseproject/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.yts.baseproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SparseArray
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.yts.baseproject.databinding.MainViewBinding
+import com.yts.baseproject.ui.BackDoubleClickFinishActivity
+import com.yts.baseproject.viewmodel.BaseViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BackDoubleClickFinishActivity<MainViewBinding>() {
+    override fun onLayoutId(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun setupViewModel(): SparseArray<ViewModel> {
+        val setupViewModel = SparseArray<ViewModel>()
+        setupViewModel.put(BR.model, ViewModelProvider(this).get(BaseViewModel::class.java))
+        return setupViewModel
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
+
+    override fun observer() {
     }
 }
